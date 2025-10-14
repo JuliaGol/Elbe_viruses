1375d785907745c14e9e6069eb4a1f9f0d4406bb
JuliaGol <79660268+JuliaGol@users.noreply.github.com>
2025-10-14 21:49:14 +0200

    Add files via upload

---
+++ IsoelectricPoint.R
@@ -0,0 +1,37 @@
+#install.packages("seqinr")
+library(seqinr)
+install.packages(c("foreach", "doParallel"))
+library(doParallel)
+library(foreach)
+# specifies that you will be manually setting the cores
+options(future.availablecores.methods = "mc.cores")
+# SLURM_CPUS_PER_TASK is the amount of cores specified in the job environment
+options(mc.cores = Sys.getenv("SLURM_CPUS_PER_TASK"))
+#mc.cores <- detectCores()
+cluster <- makeCluster(mc.cores)
+registerDoParallel(cluster)
+#setwd("C:/Users/jgolebiowska/Documents/IGB_phd/BICEST/virus/viral_genes")
+myProts <- read.fasta(file = 'cd /scratch/jgolebiowska/viruses_BICEST/Combined_phagegenes_GT.faa', seqtype = "AA")
+headers <- c()
+IP <- c()
+# Use foreach and %dopar% to run the loop in parallel
+results <- foreach(prot=iter(myProts), .combine='rbind') %dopar% {
+  # Store the results
+  IP <- seqinr::computePI(prot)
+  # IP <-c(IP,res)
+  header <- attr(prot,"name")
+  # headers <- c(headers, header)
+  x <- c(header, IP)
+}
+# Don't fotget to stop the cluster
+stopCluster(cl = cluster)
+# for (prot in myProts){
+# res <- computePI(prot)
+# IP <-c(IP,res)
+# header <- attr(prot,"name")
+# headers <- c(headers, header)
+# }
+df <- data.frame(results)
+colnames(df) <- c("header", "IP")
+rownames(df) <- NULL
+write.csv(df, file = "/scratch/jgolebiowska/viruses_BICEST/Isolectric_proteom.csv")
